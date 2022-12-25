ecda343552b4c3958b26fd0ae59332c95b195038
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2022-12-25 20:24:16 +0300

    Initial Commit

---
+++ AwardsCollectionSwiftUI/AwardsCollectionSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwardsCollectionSwiftUIApp.swift
+//  AwardsCollectionSwiftUI
+//
+//  Created by jopootrivatel on 25.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct AwardsCollectionSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
