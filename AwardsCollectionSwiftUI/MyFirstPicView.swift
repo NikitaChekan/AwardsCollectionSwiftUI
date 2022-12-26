1d5cb5c4d73e01a095446ba278b9a9b814ee714d
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2022-12-26 21:19:48 +0300

    create MyFirstPicView

---
+++ AwardsCollectionSwiftUI/MyFirstPicView.swift
@@ -0,0 +1,20 @@
+//
+//  MyFirstPic.swift
+//  AwardsCollectionSwiftUI
+//
+//  Created by jopootrivatel on 26.12.2022.
+//
+
+import SwiftUI
+
+struct MyFirstPicView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MyFirstPic_Previews: PreviewProvider {
+    static var previews: some View {
+        MyFirstPicView()
+    }
+}
