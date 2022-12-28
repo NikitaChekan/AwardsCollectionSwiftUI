a395443b0f89a802b236945b3adadd5388995363
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2022-12-28 21:33:14 +0300

    realisation AwardsView with native Grid Stack

--- AwardsCollectionSwiftUI/AwardsView.swift
+++ AwardsCollectionSwiftUI/AwardsView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct AwardsView: View {
     private let awards = Award.getAwards().filter { $0.awarded }
+    private let columns = [GridItem(.adaptive(minimum: 160, maximum: 200))]
     
     var body: some View {
         NavigationStack {
-            CustomGridView(items: awards, columns: 2) { award in
-                VStack {
-                    award.awardView
-                    Text(award.title)
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(awards, id: \.title) { award in
+                        award.awardView
+                        Text(award.title)
+                    }
                 }
             }
             .navigationTitle("Your awards: \(awards.count)")
@@ -28,3 +31,25 @@ struct AwardsView_Previews: PreviewProvider {
         AwardsView()
     }
 }
+
+//struct AwardsView: View {
+//    private let awards = Award.getAwards().filter { $0.awarded }
+//
+//    var body: some View {
+//        NavigationStack {
+//            CustomGridView(items: awards, columns: 2) { award in
+//                VStack {
+//                    award.awardView
+//                    Text(award.title)
+//                }
+//            }
+//            .navigationTitle("Your awards: \(awards.count)")
+//        }
+//    }
+//}
+//
+//struct AwardsView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        AwardsView()
+//    }
+//}
